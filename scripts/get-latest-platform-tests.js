fc129b5d1037e32d2330b4d248a52f332c55d26e
Sebastian Mayr <sebmaster16@gmail.com>
2015-04-30 10:37:17 +0100

    Also lint the scripts folder

--- scripts/get-latest-platform-tests.js
+++ scripts/get-latest-platform-tests.js
@@ -1,15 +1,16 @@
-﻿"use strict";
+"use strict";
 
 const path = require("path");
 const fs = require("fs");
 const request = require("request");
 
+const sourceDir = "https://raw.githubusercontent.com/w3c/web-platform-tests/master/url/";
 const targetDir = path.resolve(__dirname, "..", "test", "web-platform-tests");
 
-request.get("https://raw.githubusercontent.com/w3c/web-platform-tests/master/url/urltestdata.txt")
+request.get(sourceDir + "urltestdata.txt")
   .pipe(fs.createWriteStream(path.join(targetDir, "urltestdata.txt")));
 
-request.get("https://raw.githubusercontent.com/w3c/web-platform-tests/master/url/urltestparser.js", function (err, resp, body) {
+request.get(sourceDir + "urltestparser.js", function (err, resp, body) {
   if (err) {
     console.error(err);
     process.exit(1);
@@ -18,7 +19,7 @@ request.get("https://raw.githubusercontent.com/w3c/web-platform-tests/master/url
     console.log("GitHub returned non-successful status code (" + resp.statusCode + ")");
     process.exit(1);
   }
-  
+
   body += "\nmodule.exports = URLTestParser;\n";
 
   fs.writeFile(path.join(targetDir, "urltestparser.js"), body);
