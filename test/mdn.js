ffa6e9fce1601c94967eb1e282ccf8a7b61bef12
Domenic Denicola <d@domenic.me>
2016-04-01 16:16:31 -0400

    Move MDN examples to another file
    They're not web platform tests, so they shouldn't be in that file.

---
+++ test/mdn.js
@@ -0,0 +1,37 @@
+"use strict";
+/*global specify */
+
+const assert = require("assert");
+const URL = require("..").URL;
+
+specify("Checking all examples on MDN pass", () => {
+  const a = new URL("/", "https://developer.mozilla.org");
+  assert.strictEqual(a.href, "https://developer.mozilla.org/");
+
+  const b = new URL("https://developer.mozilla.org");
+  assert.strictEqual(b.href, "https://developer.mozilla.org/");
+
+  const c = new URL("en-US/docs", b);
+  assert.strictEqual(c.href, "https://developer.mozilla.org/en-US/docs");
+
+  const d = new URL("/en-US/docs", b);
+  assert.strictEqual(d.href, "https://developer.mozilla.org/en-US/docs");
+
+  const f = new URL("/en-US/docs", d);
+  assert.strictEqual(f.href, "https://developer.mozilla.org/en-US/docs");
+
+  const g = new URL("/en-US/docs", "https://developer.mozilla.org/fr-FR/toto");
+  assert.strictEqual(g.href, "https://developer.mozilla.org/en-US/docs");
+
+  const h = new URL("/en-US/docs", a);
+  assert.strictEqual(h.href, "https://developer.mozilla.org/en-US/docs");
+
+  assert.throws(() => new URL("/en-US/docs", ""));
+  assert.throws(() => new URL("/en-US/docs"));
+
+  const k = new URL("http://www.example.com", "https://developers.mozilla.com");
+  assert.strictEqual(k.href, "http://www.example.com/");
+
+  const l = new URL("http://www.example.com", b);
+  assert.strictEqual(l.href, "http://www.example.com/");
+});
--- test/web-platform.js
+++ test/web-platform.js
@@ -54,37 +54,3 @@ describe("To-upstream tests", () => {
     it("Parsing: <" + expected.input + "> against <" + expected.base + ">", testURL(expected));
   }
 });
-
-describe("MDN examples", () => {
-  it("Checking all examples on MDN pass", () => {
-    const a = new URL("/", "https://developer.mozilla.org");
-    assert.strictEqual(a.href, "https://developer.mozilla.org/");
-
-    const b = new URL("https://developer.mozilla.org");
-    assert.strictEqual(b.href, "https://developer.mozilla.org/");
-
-    const c = new URL("en-US/docs", b);
-    assert.strictEqual(c.href, "https://developer.mozilla.org/en-US/docs");
-
-    const d = new URL("/en-US/docs", b);
-    assert.strictEqual(d.href, "https://developer.mozilla.org/en-US/docs");
-
-    const f = new URL("/en-US/docs", d);
-    assert.strictEqual(f.href, "https://developer.mozilla.org/en-US/docs");
-
-    const g = new URL("/en-US/docs", "https://developer.mozilla.org/fr-FR/toto");
-    assert.strictEqual(g.href, "https://developer.mozilla.org/en-US/docs");
-
-    const h = new URL("/en-US/docs", a);
-    assert.strictEqual(h.href, "https://developer.mozilla.org/en-US/docs");
-
-    assert.throws(() => new URL("/en-US/docs", ""));
-    assert.throws(() => new URL("/en-US/docs"));
-
-    const k = new URL("http://www.example.com", "https://developers.mozilla.com");
-    assert.strictEqual(k.href, "http://www.example.com/");
-
-    const l = new URL("http://www.example.com", b);
-    assert.strictEqual(l.href, "http://www.example.com/");
-  });
-});
