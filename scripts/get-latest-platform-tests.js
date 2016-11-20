3ed2181896fba8247aba8f139b427eecc6d48061
Sebastian Mayr <github@smayr.name>
2016-11-20 04:13:25 +0100

    Update web-platform-tests

--- scripts/get-latest-platform-tests.js
+++ scripts/get-latest-platform-tests.js
@@ -15,7 +15,7 @@ const request = require("request");
 // 1. Go to https://github.com/w3c/web-platform-tests/blob/master/url/urltestdata.json
 // 2. Press "y" on your keyboard to get a permalink
 // 3. Copy the commit hash
-const commitHash = "4ec7252a7a16d8ff3dc0b02b2f4c858b4bff9e9b";
+const commitHash = "39cbd1fb20f9febd40460725c8837e3175b662e9";
 
 const sourceURL = `https://raw.githubusercontent.com/w3c/web-platform-tests/${commitHash}/url/urltestdata.json`;
 const setterSourceURL = `https://raw.githubusercontent.com/w3c/web-platform-tests/${commitHash}/url/setters_tests.json`;
