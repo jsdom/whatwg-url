jsdom/whatwg-url

commit 8849b6208199681a2aa7ef1a08e5683d47d54b36

Ensure pop path does not pop for file URLs

src/url-state-machine.js
@@ -428,6 +428,12 @@ function trimTabAndNewline(url) {
   return url.replace(/\u0009|\u000A|\u000D/g, "");
 }
 
+function popPath(url) {
+  if (url.scheme !== "file" || !url.path.filter((seg) => /^[A-Za-z](|:)$/.test(seg)).length) {
+    url.path.pop();
+  }
+}
+
 function URLStateMachine(input, base, encodingOverride, url, stateOverride) {
   this.pointer = 0;
   this.input = input;
@@ -832,7 +838,7 @@ URLStateMachine.prototype["parse file"] = function parseFile(c) {
           [p("/"), p("\\"), p("?"), p("#")].indexOf(this.input[this.pointer + 2]) === -1) {
         this.url.host = this.base.host;
         this.url.path = this.base.path.slice();
-        this.url.path.pop();
+        popPath(this.url);
       } else {
         this.parseError = true;
       }
@@ -911,7 +917,7 @@ URLStateMachine.prototype["parse path"] = function parsePath(c) {
     }
 
     if (isDoubleDot(this.buffer)) {
-      this.url.path.pop();
+      popPath(this.url);
       if (c !== p("/") && !(specialSchemas[this.url.scheme] !== undefined && c === p("\\"))) {
         this.url.path.push("");
       }

commit e7fe858dddfecdeb253592aafc67cfcb25a65038

Fix file URL resolution when Windows drive letters are present
This regressed in 8849b6208199681a2aa7ef1a08e5683d47d54b36 due to an incorrect interpretation of "shorten the path"; see https://github.com/whatwg/url/pull/174.

src/url-state-machine.js
@@ -429,9 +429,19 @@ function trimTabAndNewline(url) {
 }
 
 function shortenPath(url) {
-  if (url.scheme !== "file" || !url.path.filter(seg => /^[A-Za-z](|:)$/.test(seg)).length) {
-    url.path.pop();
+  const path = url.path;
+  if (path.length === 0) {
+    return;
   }
+  if (url.scheme === "file" && path.length === 1 && isNormalizedWindowsDriveLetter(path[0])) {
+    return;
+  }
+
+  path.pop();
+}
+
+function isNormalizedWindowsDriveLetter(string) {
+  return /^[A-Za-z]:$/.test(string);
 }
 
 function URLStateMachine(input, base, encodingOverride, url, stateOverride) {