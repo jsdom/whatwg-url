5f3e2d20f6c7abd34f91e1981ac7eeae72ddfbe0
Domenic Denicola <d@domenic.me>
2017-01-03 12:54:33 -0500

    Spec update: stop decoding all %2es in paths
    This follows https://github.com/whatwg/url/pull/156.

--- scripts/get-latest-platform-tests.js
+++ scripts/get-latest-platform-tests.js
@@ -15,7 +15,7 @@ const request = require("request");
 // 1. Go to https://github.com/w3c/web-platform-tests/tree/master/url
 // 2. Press "y" on your keyboard to get a permalink
 // 3. Copy the commit hash
-const commitHash = "e0012406859014e8f31dbaf12122d0cd10249db4";
+const commitHash = "d93247d5cb7d70f80da8b154a171f4e3d50969f4";
 
 const sourceURL = `https://raw.githubusercontent.com/w3c/web-platform-tests/${commitHash}/url/urltestdata.json`;
 const setterSourceURL = `https://raw.githubusercontent.com/w3c/web-platform-tests/${commitHash}/url/setters_tests.json`;
