91bca9e1aa53e7cd77fdaeefddd1aa96757faaf4
Domenic Denicola <d@domenic.me>
2017-01-03 12:44:03 -0500

    Update to master commit hash for tests

--- scripts/get-latest-platform-tests.js
+++ scripts/get-latest-platform-tests.js
@@ -15,7 +15,7 @@ const request = require("request");
 // 1. Go to https://github.com/w3c/web-platform-tests/tree/master/url
 // 2. Press "y" on your keyboard to get a permalink
 // 3. Copy the commit hash
-const commitHash = "f89847d4df8bb732a51c0669b3d4fa6718c71e54";
+const commitHash = "e0012406859014e8f31dbaf12122d0cd10249db4";
 
 const sourceURL = `https://raw.githubusercontent.com/w3c/web-platform-tests/${commitHash}/url/urltestdata.json`;
 const setterSourceURL = `https://raw.githubusercontent.com/w3c/web-platform-tests/${commitHash}/url/setters_tests.json`;
