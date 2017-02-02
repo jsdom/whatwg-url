12a5be1dc960a87b97d4c5227839d14ef63eac37
Domenic Denicola <d@domenic.me>
2017-02-02 08:39:23 -0800

    Sync with new web platform tests, which we pass
    Includes https://github.com/w3c/web-platform-tests/pull/4701.

--- scripts/get-latest-platform-tests.js
+++ scripts/get-latest-platform-tests.js
@@ -14,7 +14,7 @@ const request = require("request");
 // 1. Go to https://github.com/w3c/web-platform-tests/tree/master/url
 // 2. Press "y" on your keyboard to get a permalink
 // 3. Copy the commit hash
-const commitHash = "8be87380efb01f5fde485511b8df80fce93090d5";
+const commitHash = "61791654095a51a6e4fa6ee3d929f7381b32e388";
 
 const sourceURL = `https://raw.githubusercontent.com/w3c/web-platform-tests/${commitHash}/url/urltestdata.json`;
 const setterSourceURL = `https://raw.githubusercontent.com/w3c/web-platform-tests/${commitHash}/url/setters_tests.json`;
