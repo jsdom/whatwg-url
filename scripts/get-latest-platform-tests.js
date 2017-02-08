a2d83196b21af64d8f2930040b16462371019333
Domenic Denicola <d@domenic.me>
2017-02-08 12:43:50 -0500

    Spec update: ignore repeated slashes in the file scheme
    Includes a fix for the pointer decrease noted at https://github.com/whatwg/url/pull/234/files#r100125036

--- scripts/get-latest-platform-tests.js
+++ scripts/get-latest-platform-tests.js
@@ -16,8 +16,8 @@ const request = require("request");
 // 3. Copy the commit hash
 const commitHash = "3c090ebc321c78a0977e4980c1db707cc6362b93";
 
-const sourceURL = `https://raw.githubusercontent.com/w3c/web-platform-tests/${commitHash}/url/urltestdata.json`;
-const setterSourceURL = `https://raw.githubusercontent.com/w3c/web-platform-tests/${commitHash}/url/setters_tests.json`;
+const sourceURL = `https://raw.githubusercontent.com/watilde/web-platform-tests/800055d3ddfcf570bd1cee2e8eca232ac3a402f4/url/urltestdata.json`;
+const setterSourceURL = `https://raw.githubusercontent.com/watilde/web-platform-tests/800055d3ddfcf570bd1cee2e8eca232ac3a402f4/url/setters_tests.json`;
 
 const targetDir = path.resolve(__dirname, "..", "test", "web-platform-tests");
 
