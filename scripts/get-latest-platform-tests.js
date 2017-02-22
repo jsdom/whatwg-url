4fe068e07553701f84077407b7cf89f9cc2f2690
Domenic Denicola <d@domenic.me>
2017-02-22 12:43:08 -0500

    Spec update: remove javascript URL special case in the hash setter
    Follows https://github.com/whatwg/url/pull/254.

--- scripts/get-latest-platform-tests.js
+++ scripts/get-latest-platform-tests.js
@@ -14,7 +14,7 @@ const request = require("request");
 // 1. Go to https://github.com/w3c/web-platform-tests/tree/master/url
 // 2. Press "y" on your keyboard to get a permalink
 // 3. Copy the commit hash
-const commitHash = "9e33ff697b9bb0b2b0c160ae34c145f9a78ae04d";
+const commitHash = "e8aa61d80e3971489ace4355ee970d30c09b615e";
 
 const sourceURL = `https://raw.githubusercontent.com/w3c/web-platform-tests/${commitHash}/url/urltestdata.json`;
 const setterSourceURL = `https://raw.githubusercontent.com/w3c/web-platform-tests/${commitHash}/url/setters_tests.json`;
