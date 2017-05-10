a067aa7e6239fa95924612b919353558dfa98652
Domenic Denicola <d@domenic.me>
2017-05-10 14:48:21 -0400

    Update hashes

--- scripts/get-latest-platform-tests.js
+++ scripts/get-latest-platform-tests.js
@@ -14,7 +14,7 @@ const request = require("request");
 // 1. Go to https://github.com/w3c/web-platform-tests/tree/master/url
 // 2. Press "y" on your keyboard to get a permalink
 // 3. Copy the commit hash
-const commitHash = "cfd901b9c07d5f43fcbd0464815d9e26bd48f365";
+const commitHash = "28541bb338e33fb79bc6bf64330b62fac2eca325";
 
 const sourceURL = `https://raw.githubusercontent.com/w3c/web-platform-tests/${commitHash}/url/urltestdata.json`;
 const setterSourceURL = `https://raw.githubusercontent.com/w3c/web-platform-tests/${commitHash}/url/setters_tests.json`;
