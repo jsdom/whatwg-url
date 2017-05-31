c65a090f783ab775fd95c0b2c65699fe864ace38
Domenic Denicola <d@domenic.me>
2017-05-31 15:51:38 -0400

    Test using new test cases

--- scripts/get-latest-platform-tests.js
+++ scripts/get-latest-platform-tests.js
@@ -14,10 +14,11 @@ const request = require("request");
 // 1. Go to https://github.com/w3c/web-platform-tests/tree/master/url
 // 2. Press "y" on your keyboard to get a permalink
 // 3. Copy the commit hash
-const commitHash = "34435a45164168db1403842ea7210bc99c4ef35b";
+const commitHash = "762a7a6ec01142fe9cfe6979034582bffdd3c66f";
 
 const sourceURL = `https://raw.githubusercontent.com/w3c/web-platform-tests/${commitHash}/url/urltestdata.json`;
 const setterSourceURL = `https://raw.githubusercontent.com/w3c/web-platform-tests/${commitHash}/url/setters_tests.json`;
+const toASCIISourceURL = `https://raw.githubusercontent.com/w3c/web-platform-tests/${commitHash}/url/toascii.json`;
 
 const targetDir = path.resolve(__dirname, "..", "test", "web-platform-tests");
 
@@ -26,3 +27,6 @@ request.get(sourceURL)
 
 request.get(setterSourceURL)
   .pipe(fs.createWriteStream(path.resolve(targetDir, "setters_tests.json")));
+
+request.get(toASCIISourceURL)
+  .pipe(fs.createWriteStream(path.resolve(targetDir, "toascii.json")));
