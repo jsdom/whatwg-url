d5ec60b01d1266bfd46652b7b274da5e58fec037
Timothy Gu <timothygu99@gmail.com>
2017-06-28 19:16:16 -0400

    Update webidl2js and webidl-conversions
    Pulled from #87. This increases the minimum Node.js version requirement to 6.

--- scripts/convert-idl.js
+++ scripts/convert-idl.js
@@ -1,11 +1,18 @@
 "use strict";
-const fs = require("fs");
 const path = require("path");
-const webidl2js = require("webidl2js");
+const Transformer = require("webidl2js");
 
-const idlFilePath = path.resolve(__dirname, "../src/URL.idl");
-const outputDir = path.resolve(__dirname, "../lib");
+const srcDir = path.resolve(__dirname, "../src");
 const implDir = path.resolve(__dirname, "../lib");
+const outputDir = path.resolve(__dirname, "../lib");
 
-const src = fs.readFileSync(idlFilePath, { encoding: "utf-8" });
-webidl2js.generate(src, outputDir, implDir, { implSuffix: "-impl" });
+const transformer = new Transformer({
+  implSuffix: "-impl"
+});
+
+transformer.addSource(srcDir, implDir);
+transformer.generate(outputDir)
+  .catch(err => {
+    console.error(err.stack);
+    process.exit(0);
+  });
