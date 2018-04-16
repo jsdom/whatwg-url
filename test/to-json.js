c819c6312bceea969b3f54572db62abb6e44813b
Timothy Gu <timothygu99@gmail.com>
2018-04-16 09:08:00 -0700

    Enable more WPTs

--- test/to-json.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const assert = require("assert");
-const { URL } = require("..");
-
-test("toJSON() works as expected", () => {
-  const u = new URL("/", "https://developer.mozilla.org");
-  assert.strictEqual(u.toJSON(), "https://developer.mozilla.org/");
-  assert.strictEqual(JSON.stringify(u), "\"https://developer.mozilla.org/\"");
-});
