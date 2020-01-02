9f6537c7f52154b5f4745b792d378bf313ac60a2
Pierre-Marie Dartus <dartus.pierremarie@gmail.com>
2020-01-02 21:09:42 +0100

    Upgrade webidl2js, WPTs, and dependencies
    This upgrades to the latest version of webidl2js, and as such, exposes the webidl2js-wrapper submodule out of the package for use by environments like jsdom.
    
    This also upgrades all other dependencies and dev dependencies, and sync with the latest web platform tests.
    
    Finally, it raises the minimum required Node.js version to v10.

--- scripts/get-latest-platform-tests.js
+++ scripts/get-latest-platform-tests.js
@@ -4,9 +4,14 @@ if (process.env.NO_UPDATE) {
   process.exit(0);
 }
 
-const path = require("path");
 const fs = require("fs");
+const path = require("path");
+const util = require("util");
+const stream = require("stream");
+
 const got = require("got");
+
+const pipeline = util.promisify(stream.pipeline);
 
 process.on("unhandledRejection", err => {
   throw err;
@@ -18,7 +23,7 @@ process.on("unhandledRejection", err => {
 // 1. Go to https://github.com/w3c/web-platform-tests/tree/master/url
 // 2. Press "y" on your keyboard to get a permalink
 // 3. Copy the commit hash
-const commitHash = "fd4814ce7fcc4d6c23c73aac62daa8299feb0510";
+const commitHash = "6fbd872e9ac5fe60e32946bc9b318be6eeada123";
 
 const urlPrefix = `https://raw.githubusercontent.com/web-platform-tests/wpt/${commitHash}/url/`;
 const targetDir = path.resolve(__dirname, "..", "test", "web-platform-tests");
@@ -40,6 +45,8 @@ for (const file of [
   "urlsearchparams-sort.any.js",
   "urlsearchparams-stringifier.any.js"
 ]) {
-  got(`${urlPrefix}${file}`, { stream: true })
-    .pipe(fs.createWriteStream(path.resolve(targetDir, file)));
+  pipeline(
+    got.stream(`${urlPrefix}${file}`),
+    fs.createWriteStream(path.resolve(targetDir, file))
+  );
 }
