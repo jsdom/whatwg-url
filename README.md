3b7677af878619c9b265d4d544e897ee6a460075
Anne van Kesteren <annevk@annevk.nl>
2020-05-11 19:49:25 +0200

    Refactor "parse relative"
    Follows https://github.com/whatwg/url/pull/509.

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ whatwg-url is a full implementation of the WHATWG [URL Standard](https://url.spe
 
 ## Specification conformance
 
-whatwg-url is currently up to date with the URL spec up to commit [cceb435](https://github.com/whatwg/url/commit/cceb4356cca233b6dfdaabd888263157b2204e44).
+whatwg-url is currently up to date with the URL spec up to commit [e5b1dd7](https://github.com/whatwg/url/commit/e5b1dd76df13183b7f123d15880dbdc6a2f37c71).
 
 For `file:` URLs, whose [origin is left unspecified](https://url.spec.whatwg.org/#concept-url-origin), whatwg-url chooses to use a new opaque origin (which serializes to `"null"`).
 
--- src/url-state-machine.js
+++ src/url-state-machine.js
@@ -679,32 +679,8 @@ URLStateMachine.prototype["parse path or authority"] = function parsePathOrAutho
 
 URLStateMachine.prototype["parse relative"] = function parseRelative(c) {
   this.url.scheme = this.base.scheme;
-  if (isNaN(c)) {
-    this.url.username = this.base.username;
-    this.url.password = this.base.password;
-    this.url.host = this.base.host;
-    this.url.port = this.base.port;
-    this.url.path = this.base.path.slice();
-    this.url.query = this.base.query;
-  } else if (c === p("/")) {
+  if (c === p("/")) {
     this.state = "relative slash";
-  } else if (c === p("?")) {
-    this.url.username = this.base.username;
-    this.url.password = this.base.password;
-    this.url.host = this.base.host;
-    this.url.port = this.base.port;
-    this.url.path = this.base.path.slice();
-    this.url.query = "";
-    this.state = "query";
-  } else if (c === p("#")) {
-    this.url.username = this.base.username;
-    this.url.password = this.base.password;
-    this.url.host = this.base.host;
-    this.url.port = this.base.port;
-    this.url.path = this.base.path.slice();
-    this.url.query = this.base.query;
-    this.url.fragment = "";
-    this.state = "fragment";
   } else if (isSpecial(this.url) && c === p("\\")) {
     this.parseError = true;
     this.state = "relative slash";
@@ -713,10 +689,20 @@ URLStateMachine.prototype["parse relative"] = function parseRelative(c) {
     this.url.password = this.base.password;
     this.url.host = this.base.host;
     this.url.port = this.base.port;
-    this.url.path = this.base.path.slice(0, this.base.path.length - 1);
-
-    this.state = "path";
-    --this.pointer;
+    this.url.path = this.base.path.slice();
+    this.url.query = this.base.query;
+    if (c === p("?")) {
+      this.url.query = "";
+      this.state = "query";
+    } else if (c === p("#")) {
+      this.url.fragment = "";
+      this.state = "fragment";
+    } else if (!isNaN(c)) {
+      this.url.query = null;
+      this.url.path.pop();
+      this.state = "path";
+      --this.pointer;
+    }
   }
 
   return true;
