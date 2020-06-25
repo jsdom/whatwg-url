fdeeea1d71ec8015bdcfa0437d6cc9bda30604e6
Domenic Denicola <d@domenic.me>
2020-06-25 15:31:55 -0400

    Spec update: add the component percent-encode set
    This has no observable behavior changes and the component percent-encode set isn't even used by the rest of the implementation directly. However, it keeps us in sync with the spec, and it isn't totally dead code since it is used indirectly.
    
    Also fixes various spec URLs in comments that were accidentally left pointing a PR preview in cf7a7c1c29f6853faf79f50c97e0767da2182a52.

--- src/percent-encoding.js
+++ src/percent-encoding.js
@@ -6,7 +6,7 @@ function p(char) {
   return char.codePointAt(0);
 }
 
-// https://whatpr.org/url/518.html#percent-encode
+// https://url.spec.whatwg.org/#percent-encode
 function percentEncode(c) {
   let hex = c.toString(16).toUpperCase();
   if (hex.length === 1) {
@@ -16,7 +16,7 @@ function percentEncode(c) {
   return "%" + hex;
 }
 
-// https://whatpr.org/url/518.html#percent-decode
+// https://url.spec.whatwg.org/#percent-decode
 function percentDecodeBytes(input) {
   const output = new Uint8Array(input.byteLength);
   let outputIndex = 0;
@@ -36,58 +36,61 @@ function percentDecodeBytes(input) {
   return output.slice(0, outputIndex);
 }
 
-// https://whatpr.org/url/518.html#string-percent-decode
+// https://url.spec.whatwg.org/#string-percent-decode
 function percentDecodeString(input) {
   const bytes = utf8Encode(input);
   return percentDecodeBytes(bytes);
 }
 
-// https://whatpr.org/url/518.html#c0-control-percent-encode-set
+// https://url.spec.whatwg.org/#c0-control-percent-encode-set
 function isC0ControlPercentEncode(c) {
   return c <= 0x1F || c > 0x7E;
 }
 
-// https://whatpr.org/url/518.html#fragment-percent-encode-set
+// https://url.spec.whatwg.org/#fragment-percent-encode-set
 const extraFragmentPercentEncodeSet = new Set([p(" "), p("\""), p("<"), p(">"), p("`")]);
 function isFragmentPercentEncode(c) {
   return isC0ControlPercentEncode(c) || extraFragmentPercentEncodeSet.has(c);
 }
 
-// https://whatpr.org/url/518.html#query-percent-encode-set
+// https://url.spec.whatwg.org/#query-percent-encode-set
 const extraQueryPercentEncodeSet = new Set([p(" "), p("\""), p("#"), p("<"), p(">")]);
 function isQueryPercentEncode(c) {
   return isC0ControlPercentEncode(c) || extraQueryPercentEncodeSet.has(c);
 }
 
-// https://whatpr.org/url/518.html#special-query-percent-encode-set
+// https://url.spec.whatwg.org/#special-query-percent-encode-set
 function isSpecialQueryPercentEncode(c) {
   return isQueryPercentEncode(c) || c === p("'");
 }
 
-// https://whatpr.org/url/518.html#path-percent-encode-set
+// https://url.spec.whatwg.org/#path-percent-encode-set
 const extraPathPercentEncodeSet = new Set([p("?"), p("`"), p("{"), p("}")]);
 function isPathPercentEncode(c) {
   return isQueryPercentEncode(c) || extraPathPercentEncodeSet.has(c);
 }
 
-// https://whatpr.org/url/518.html#userinfo-percent-encode-set
+// https://url.spec.whatwg.org/#userinfo-percent-encode-set
 const extraUserinfoPercentEncodeSet =
   new Set([p("/"), p(":"), p(";"), p("="), p("@"), p("["), p("\\"), p("]"), p("^"), p("|")]);
 function isUserinfoPercentEncode(c) {
   return isPathPercentEncode(c) || extraUserinfoPercentEncodeSet.has(c);
 }
 
-// https://whatpr.org/url/518.html#application-x-www-form-urlencoded-percent-encode-set
-const extraURLEncodedPercentEncodeSet = new Set([
-  p("!"), p("$"), p("%"), p("&"), p("'"),
-  p("("), p(")"), p("+"), p(","), p("~")
-]);
-function isURLEncodedPercentEncode(c) {
-  return isUserinfoPercentEncode(c) || extraURLEncodedPercentEncodeSet.has(c);
+// https://url.spec.whatwg.org/#component-percent-encode-set
+const extraComponentPercentEncodeSet = new Set([p("$"), p("%"), p("&"), p("+"), p(",")]);
+function isComponentPercentEncode(c) {
+  return isUserinfoPercentEncode(c) || extraComponentPercentEncodeSet.has(c);
 }
 
-// https://whatpr.org/url/518.html#code-point-percent-encode-after-encoding
-// https://whatpr.org/url/518.html#utf-8-percent-encode
+// https://url.spec.whatwg.org/#application-x-www-form-urlencoded-percent-encode-set
+const extraURLEncodedPercentEncodeSet = new Set([p("!"), p("'"), p("("), p(")"), p("~")]);
+function isURLEncodedPercentEncode(c) {
+  return isComponentPercentEncode(c) || extraURLEncodedPercentEncodeSet.has(c);
+}
+
+// https://url.spec.whatwg.org/#code-point-percent-encode-after-encoding
+// https://url.spec.whatwg.org/#utf-8-percent-encode
 // Assuming encoding is always utf-8 allows us to trim one of the logic branches. TODO: support encoding.
 // The "-Internal" variant here has code points as JS strings. The external version used by other files has code points
 // as JS numbers, like the rest of the codebase.
@@ -110,8 +113,8 @@ function utf8PercentEncodeCodePoint(codePoint, percentEncodePredicate) {
   return utf8PercentEncodeCodePointInternal(String.fromCodePoint(codePoint), percentEncodePredicate);
 }
 
-// https://whatpr.org/url/518.html#string-percent-encode-after-encoding
-// https://whatpr.org/url/518.html#string-utf-8-percent-encode
+// https://url.spec.whatwg.org/#string-percent-encode-after-encoding
+// https://url.spec.whatwg.org/#string-utf-8-percent-encode
 function utf8PercentEncodeString(input, percentEncodePredicate, spaceAsPlus = false) {
   let output = "";
   for (const codePoint of input) {
