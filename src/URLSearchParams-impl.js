eda5f54546ffac2db63d981cdfd7eded533601d5
Damian Stasik <visualfanatic@users.noreply.github.com>
2021-05-06 20:55:17 +0200

    Replace full lodash install with lodash.sortby

--- src/URLSearchParams-impl.js
+++ src/URLSearchParams-impl.js
@@ -1,5 +1,5 @@
 "use strict";
-const stableSortBy = require("lodash/sortBy");
+const stableSortBy = require("lodash.sortBy");
 const urlencoded = require("./urlencoded");
 
 exports.implementation = class URLSearchParamsImpl {
