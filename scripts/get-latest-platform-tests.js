7c9b9e87311ee39c70a57a49dca3b47af58ae67a
Domenic Denicola <d@domenic.me>
2021-08-05 11:54:33 -0400

    Reject non-IPv4 hostnames that end in numbers
    Follows https://github.com/whatwg/url/pull/619.

--- scripts/get-latest-platform-tests.js
+++ scripts/get-latest-platform-tests.js
@@ -20,10 +20,10 @@ process.on("unhandledRejection", err => {
 // Pin to specific version, reflecting the spec version in the readme.
 //
 // To get the latest commit:
-// 1. Go to https://github.com/w3c/web-platform-tests/tree/master/url
+// 1. Go to https://github.com/web-platform-tests/wpt/tree/master/url
 // 2. Press "y" on your keyboard to get a permalink
 // 3. Copy the commit hash
-const commitHash = "706f8d143464de7d923dab391f3eab2469f042ae";
+const commitHash = "eb8bdce552a2a9b10010f7755ed9d8d0773d548e";
 
 const urlPrefix = `https://raw.githubusercontent.com/web-platform-tests/wpt/${commitHash}/url/`;
 const targetDir = path.resolve(__dirname, "..", "test", "web-platform-tests");
