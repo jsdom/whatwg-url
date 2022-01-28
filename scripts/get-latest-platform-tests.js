a4cb13309246ca9ecf03404fdbf0d23ecaf114dd
Karl <5254025+karwa@users.noreply.github.com>
2022-01-28 20:16:46 +0100

    Split forbidden host/domain code-points
    Follows https://github.com/whatwg/url/pull/685.

--- scripts/get-latest-platform-tests.js
+++ scripts/get-latest-platform-tests.js
@@ -23,7 +23,7 @@ process.on("unhandledRejection", err => {
 // 1. Go to https://github.com/web-platform-tests/wpt/tree/master/url
 // 2. Press "y" on your keyboard to get a permalink
 // 3. Copy the commit hash
-const commitHash = "5acc42721ce5811462acc297bff75d33f999cd8f";
+const commitHash = "681773c2a927f936b71b4163469942080fbd5fab";
 
 const urlPrefix = `https://raw.githubusercontent.com/web-platform-tests/wpt/${commitHash}/url/`;
 const targetDir = path.resolve(__dirname, "..", "test", "web-platform-tests");
