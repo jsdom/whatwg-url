8ba28f4636712f0a654b8972ba3f38b4d55ea8d2
Domenic Denicola <d@domenic.me>
2023-11-05 23:07:38 +0900

    Use official GitHub Actions for live viewer deployment

--- scripts/deploy-live-viewer.sh
+++ scripts/deploy-live-viewer.sh
@@ -6,5 +6,5 @@ cd "$(dirname "$0")"
 
 npm run build-live-viewer
 
-mkdir -p ../out
-cp -r ../live-viewer/* ../out # exclude files starting with . by using /*
+mkdir -p ../_site
+cp -r ../live-viewer/* ../_site # exclude files starting with . by using /*
