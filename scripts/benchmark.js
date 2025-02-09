528c261a02991aec46329692efa1723a6761d48a
Domenic Denicola <d@domenic.me>
2025-02-09 16:30:06 +0900

    Implement faster trimming of control characters
    Due to a V8 bug (https://issues.chromium.org/issues/42204424), negative end-of-string matches are slow, as shown in #286. We can avoid triggering this slowdown for certain inputs by just implementing the trimming using loops.
    
    Closes #286. Closes #288 by superseding it.

--- scripts/benchmark.js
+++ scripts/benchmark.js
@@ -1,14 +1,13 @@
 "use strict";
 const { URL } = require("../");
 const Benchmark = require("benchmark");
-const testData = require("../test/web-platform-tests/resources/urltestdata.json");
 
+const testData = require("../test/web-platform-tests/resources/urltestdata.json");
 const testInputs = testData.filter(c => typeof c === "object").map(c => c.input);
 
-const benchmark = new Benchmark(() => {
+runBenchmark("URL constructor with WPT data", () => {
   for (const input of testInputs) {
     try {
-      // eslint-disable-next-line no-new
       new URL(input);
     } catch {
       // intentionally empty
@@ -16,5 +15,22 @@ const benchmark = new Benchmark(() => {
   }
 });
 
-benchmark.on("cycle", e => console.log(e.target.toString()));
-benchmark.run();
+runBenchmark("long input not starting or ending with control characters (GH-286)", () => {
+  try {
+    new URL("!!" + "\u0000".repeat(100000) + "A\rA");
+  } catch {
+    // intentionally empty
+  }
+});
+
+function runBenchmark(name, fn) {
+  new Benchmark(name, fn, {
+    onComplete(event) {
+      console.log(`${name}:`);
+      console.log(`  ${event.target.hz.toFixed(0)} ops/second`);
+      console.log(`  ±${event.target.stats.rme.toFixed(2)}% relative margin of error`);
+      console.log(`  ${event.target.stats.sample.length} samples`);
+      console.log("");
+    }
+  }).run();
+}
